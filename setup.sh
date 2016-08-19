5d0a4cafc504b9e694d4c90be821d0e8f439c632
jdeboi <jdeboi@gmail.com>
2016-08-19 10:06:24 -0500

    undoing redundancy in deepdream-aws

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
-set -e
+# set -e
 
 # Disable camera, we don't have any:
 # sudo ln /dev/null /dev/raw1394
 
-git clone git@github.com:jdeboi/deepdream-aws.git
-cd deepdream-aws
-ln -s ~/caffe .
+# git clone git@github.com:jdeboi/deepdream-aws.git
+# cd deepdream-aws
+#ln -s ~/caffe .
 # Get the googlenet model file:
-wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
-mv bvlc_googlenet.caffemodel caffe/models/bvlc_googlenet/
+# wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+#mv bvlc_googlenet.caffemodel caffe/models/bvlc_googlenet/
 
 
 ######### (Daniel) Get some input image:
