921034f37c1e70c0ab8aed5dd3ba9851379b07f2
jdeboi <jdeboi@gmail.com>
2016-08-19 09:41:09 -0500

    update dns didn't work?

--- setup.sh
+++ setup.sh
@@ -10,9 +10,18 @@ ln -s ~/caffe .
 wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
 mv bvlc_googlenet.caffemodel caffe/models/bvlc_googlenet/
 
+
+######### (Daniel) Get some input image:
+wget --output-document=daniel.png https://www.gravatar.com/avatar/17d651b2d4092b7810ae23d8d94b8cbd?s=640
+convert daniel.png daniel.jpg
+
+# Run:
+nohup bash layers.sh daniel.jpg &
+#########
+
 # Get some input image:
-wget --output-document=output.jpg http://orig13.deviantart.net/90b6/f/2011/145/a/7/the_grid_by_sweetamberkins-d3h6ksq.jpg
+#wget --output-document=output.jpg http://orig13.deviantart.net/90b6/f/2011/145/a/7/the_grid_by_sweetamberkins-d3h6ksq.jpg
 # convert daniel.png daniel.jpg
 
 # Run:
-nohup bash layers.sh output.jpg &
+#nohup bash layers.sh output.jpg &
